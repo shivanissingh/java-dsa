8362f6fe63372d3dbe9e53968056c65957fdd281
shivanissingh <shivanissingh01@gmail.com>
2026-01-13 01:00:16 +0530

    All Solutions of Practice Problem 1 - LeetCode #1480 : Running Sum of 1D Array

--- 1. Language Basics/1.1 Java Basics/i. Running Sum of 1D Array/RunningSum1D.java
+++ 1. Language Basics/1.1 Java Basics/i. Running Sum of 1D Array/RunningSum1D.java
@@ -2,12 +2,38 @@ import java.util.Scanner;
 
 class RunningSum1D {
     public int[] runningSum(int[] nums){
+        /* My Approach - Optimal Solution
+        
+        int sum = 0 ;
         int[] result = new int[nums.length];
         for(int i = 0 ; i < nums.length ; i++){
-            result[i] += nums[i];
+            sum += nums[i] ;
+            result[i] = sum ;
         }
 
-        return result;
+        return result; // TC = O(n) & SC = O(n) */
+
+
+
+        /* Approach 2
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        for(int i = 1 ; i < nums.length ; i++){
+            result[i] = result[i-1] + nums[i] ;
+        }
+
+        return result;   // TC = O(n) & SC = O(n) */
+
+
+
+        //Approach 3 - More Optimized Approach by GPT
+
+        for(int i = 1 ; i < nums.length ; i++){
+            nums[i] += nums[i-1];
+        }
+
+        return nums; // TC = O(n) & SC = O(1)
+
     }
 
     public static void main(String[] args){
@@ -23,7 +49,11 @@ class RunningSum1D {
 
         RunningSum1D obj = new RunningSum1D();
 
-        obj.runningSum(nums);
+        int[] finalResult = obj.runningSum(nums);
+
+        for(int i = 0; i < size ; i++){
+            System.out.print(finalResult[i] +" ");
+        }
 
         sc.close();
 
