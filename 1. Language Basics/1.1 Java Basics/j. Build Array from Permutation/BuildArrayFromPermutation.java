b0189174a9b7ed58e3eb8ae189747e311eed1d7c
shivanissingh <shivanissingh01@gmail.com>
2026-01-14 23:50:59 +0530

    Practice Problem 2 - LeetCode #1920 : Build Array from Permutation

---
+++ 1. Language Basics/1.1 Java Basics/j. Build Array from Permutation/BuildArrayFromPermutation.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class BuildArrayFromPermutation {
+    public int[] buildArray(int[] nums){
+
+        int[] arr = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++){
+            arr[i] = nums[nums[i]];
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+
+        int[] nums = new int[size];
+
+        for(int i = 0 ; i < size ; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        BuildArrayFromPermutation obj = new BuildArrayFromPermutation();
+
+        int[] ans = obj.buildArray(nums);
+
+        for(int i = 0 ; i < size ; i++ ){
+            System.out.print(ans[i] + " ");
+        }
+
+    } 
+}
