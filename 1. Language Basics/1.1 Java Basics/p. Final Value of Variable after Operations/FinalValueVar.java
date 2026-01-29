150fca5a340084f4aa86159e4f66887fb5c259f2
shivanissingh <shivanissingh01@gmail.com>
2026-01-29 23:29:47 +0530

    Practice Problem 6 - LeetCode #1662 : Check If Two String Arrays are Equivalent

--- 1. Language Basics/1.1 Java Basics/p. Final Value of Variable after Operations/FinalValueVar.java
+++ 1. Language Basics/1.1 Java Basics/p. Final Value of Variable after Operations/FinalValueVar.java
@@ -1,3 +1,48 @@
+import java.util.Scanner;
+
+
 class FinalValueVar {
-    
+    public int finalValueAfterOperations(String[] operations){
+        int value = 0 ;
+        int x = 0;
+        for(int i = 0 ; i < operations.length ; i++){
+            if(operations[i].equals("x++")){
+                value = x++ ;
+            }
+            else if(operations[i].equals("++x")){
+                value = ++x;
+            }
+            else if(operations[i].equals("x--")){
+                value = x--;
+            }
+            else{
+                value = --x;
+            }
+        }
+
+        return value ;
+
+    }
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+
+        String[] operations = new String[size];
+
+        for(int i = 0 ; i < size ; i++){
+            operations[i] = sc.next();
+        }
+
+        FinalValueVar obj = new FinalValueVar();
+
+        int value = obj.finalValueAfterOperations(operations);
+
+        System.out.println(value);
+
+        sc.close();
+
+    }
 }
---
+++ 1. Language Basics/1.1 Java Basics/q. Check if Two String Arrays are Equivalent/TwoStringArraysEqual.java
@@ -0,0 +1,28 @@
+class TwoStringArraysEqual {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        int i = 0, j = 0;  
+        int p = 0, q = 0;   
+
+        while (i < word1.length && j < word2.length) {
+            if (word1[i].charAt(p) != word2[j].charAt(q)) {
+                return false;
+            }
+
+            p++;
+            q++;
+
+            if (p == word1[i].length()) {
+                i++;
+                p = 0;
+            }
+
+            if (q == word2[j].length()) {
+                j++;
+                q = 0;
+            }
+        }
+
+        return i == word1.length && j == word2.length;
+    } 
+
+}
