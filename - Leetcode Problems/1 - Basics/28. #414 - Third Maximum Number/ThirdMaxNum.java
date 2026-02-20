98e51fd328901cb96a62ba84aa87d0e01c5095b6
shivanissingh <shivanissingh01@gmail.com>
2026-02-20 23:17:26 +0530

    Practice Problem 28 - LeetCode #414 : Third Maximum Number

---
+++ - Leetcode Problems/1 - Basics/28. #414 - Third Maximum Number/ThirdMaxNum.java
@@ -0,0 +1,33 @@
+class ThirdMaxNum {
+    public int thirdMax(int[] nums) {
+
+        Integer first = null;
+        Integer second = null;
+        Integer third = null;
+
+        for (int num : nums) {
+
+            if ((first != null && num == first) ||
+                (second != null && num == second) ||
+                (third != null && num == third)) {
+                continue;
+            }
+
+            if (first == null || num > first) {
+                third = second;
+                second = first;
+                first = num;
+            }
+            else if (second == null || num > second) {
+                third = second;
+                second = num;
+            }
+            else if (third == null || num > third) {
+                third = num;
+            }
+        }
+
+        return third == null ? first : third;
+    }
+}
+
